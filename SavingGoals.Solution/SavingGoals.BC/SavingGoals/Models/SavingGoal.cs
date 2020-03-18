f699c9a0c3c9c44a2fec3be917812987eacadf17
Felix Z <49648575+felix-zamora-10@users.noreply.github.com>
2020-03-18 09:56:37 -0600

    Se incluyen interfaces y modelos

---
+++ SavingGoals.Solution/SavingGoals.BC/SavingGoals/Models/SavingGoal.cs
@@ -0,0 +1,11 @@
+﻿namespace SavingGoals.BC.SavingGoals.Models {
+    public class SavingGoal {
+
+        public int IdSavingGoal { get; set; }
+
+        public int Description { get; set; }
+
+        public int AmountSaved { get; set; }
+
+    }
+}
---
+++ SavingGoals.Solution/SavingGoals.BW/SavingGoals/Contracts/ISavingGoalDataAccess.cs
@@ -0,0 +1,18 @@
+﻿using SavingGoals.BC.SavingGoals.Models;
+using System.Collections.Generic;
+
+namespace SavingGoals.BW.SavingGoals.Contracts {
+    interface ISavingGoalDataAccess {
+
+        List<SavingGoal> GetSavingGoals();
+
+        SavingGoal GetSavingGoal(int idSavingGoal);
+
+        void AddSavingGoal(SavingGoal savingGoal);
+
+        void UpdateSavingGoal(SavingGoal savingGoal);
+
+        void DeleteSavingGoal(int idSavingGoal);
+
+    }
+}
